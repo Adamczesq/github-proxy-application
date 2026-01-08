093bc39bdc83cdfb86d13c2794e2e3d7de8d65e8
Adam Zbrowski <adam.zbrowski.programista@gmail.com>
2026-01-08 19:52:50 +0100

    - service method and tests improvements;

--- src/main/java/com/adamczesq/githubproxyapplication/GithubService.java
+++ src/main/java/com/adamczesq/githubproxyapplication/GithubService.java
@@ -3,6 +3,7 @@ package com.adamczesq.githubproxyapplication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 class GithubService {
@@ -13,16 +14,20 @@ class GithubService {
     }
 
     List<ProxyRepositoryResponse> getRepositories(String username) {
-        return client.fetchUserRepos(username).stream()
+        List<CompletableFuture<ProxyRepositoryResponse>> futures = client.fetchUserRepos(username).stream()
                 .filter(repo -> !repo.fork())
-                .map(repo -> {
+                .map(repo -> CompletableFuture.supplyAsync(() -> {
                     var branches = client.fetchRepoBranches(repo.owner().login(), repo.name())
                             .stream()
                             .map(branch -> new ProxyBranchResponse(branch.name(), branch.commit().sha()))
                             .toList();
 
                     return new ProxyRepositoryResponse(repo.name(), repo.owner().login(), branches);
-                })
+                }))
+                .toList();
+
+        return futures.stream()
+                .map(CompletableFuture::join)
                 .toList();
     }
 }
--- src/test/java/com/adamczesq/githubproxyapplication/GithubProxyApplicationTests.java
+++ src/test/java/com/adamczesq/githubproxyapplication/GithubProxyApplicationTests.java
@@ -3,8 +3,10 @@ package com.adamczesq.githubproxyapplication;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.apache.commons.lang3.time.StopWatch;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,6 +17,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -39,39 +42,81 @@ class GithubProxyApplicationTests {
 		wireMockServer.stop();
 	}
 
+	@BeforeEach
+	void resetWireMock() {
+		WireMock.reset();
+	}
+
 	@DynamicPropertySource
 	static void overrideProperties(DynamicPropertyRegistry registry) {
 		registry.add("github.api.url", wireMockServer::baseUrl);
 	}
 
 	@Test
-	void shouldReturnUserRepositoriesIncludingBranches() throws Exception {
+	void shouldReturnUserRepositoriesIncludingBranchesInParallel() throws Exception {
+		int delay = 1000;
+
 		stubFor(WireMock.get("/users/testuser/repos")
 				.willReturn(aResponse()
 						.withHeader("Content-Type", "application/json")
+						.withFixedDelay(delay)
 						.withBody("""
                                 [
-                                    {"name": "my-repo", "owner": {"login": "testuser"}, "fork": false},
+                                    {"name": "my-repo-1", "owner": {"login": "testuser"}, "fork": false},
+                                    {"name": "my-repo-2", "owner": {"login": "testuser"}, "fork": false},
                                     {"name": "forked-repo", "owner": {"login": "testuser"}, "fork": true}
                                 ]
                                 """)));
 
-		stubFor(WireMock.get("/repos/testuser/my-repo/branches")
+		stubFor(WireMock.get("/repos/testuser/my-repo-1/branches")
 				.willReturn(aResponse()
 						.withHeader("Content-Type", "application/json")
+						.withFixedDelay(delay)
 						.withBody("""
                                 [
                                     {"name": "main", "commit": {"sha": "111abc"}}
                                 ]
                                 """)));
 
-		mockMvc.perform(get("/api/v1/repositories/testuser")
-						.accept(MediaType.APPLICATION_JSON))
+		stubFor(WireMock.get("/repos/testuser/my-repo-2/branches")
+				.willReturn(aResponse()
+						.withHeader("Content-Type", "application/json")
+						.withFixedDelay(delay)
+						.withBody("""
+                                [
+                                    {"name": "main", "commit": {"sha": "222abc"}}
+                                ]
+                                """)));
+
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+
+		var resultActions = mockMvc.perform(get("/api/v1/repositories/testuser")
+				.accept(MediaType.APPLICATION_JSON));
+
+		stopWatch.stop();
+
+		resultActions
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.length()").value(1))
-				.andExpect(jsonPath("$[0].repositoryName").value("my-repo"))
+				.andExpect(jsonPath("$.length()").value(2))
+
+				.andExpect(jsonPath("$[0].repositoryName").value("my-repo-1"))
+				.andExpect(jsonPath("$[0].ownerLogin").value("testuser"))
 				.andExpect(jsonPath("$[0].branches[0].name").value("main"))
-				.andExpect(jsonPath("$[0].branches[0].lastCommitSha").value("111abc"));
+				.andExpect(jsonPath("$[0].branches[0].lastCommitSha").value("111abc"))
+
+				.andExpect(jsonPath("$[1].repositoryName").value("my-repo-2"))
+				.andExpect(jsonPath("$[1].branches[0].name").value("main"))
+				.andExpect(jsonPath("$[1].branches[0].lastCommitSha").value("222abc"));
+
+
+		long totalTimeMillis = stopWatch.getTime();
+
+		verify(3, getRequestedFor(urlMatching(".*")));
+
+		assertThat(totalTimeMillis)
+				.isGreaterThanOrEqualTo(2000)
+				.isLessThanOrEqualTo(3000);
 	}
 
 	@Test
@@ -86,6 +131,6 @@ class GithubProxyApplicationTests {
 						.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isNotFound())
 				.andExpect(jsonPath("$.status").value(404))
-				.andExpect(jsonPath("$.message").exists());
+				.andExpect(jsonPath("$.message").value("User not found"));
 	}
 }
